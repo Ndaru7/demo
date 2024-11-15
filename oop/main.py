88badff08f4a26ed28359fb59497b729fa639b83
daru <daru@gmail.com>
2024-11-15 13:38:50 +0700

    tambah file .gitignore

--- oop/main.py
+++ oop/main.py
@@ -1,10 +1,19 @@
-class Hero:
-    health = 100
-
-    def take_dmg(self,damage):
-        self.health -= damage
-
-
-hero1 = Hero()
-hero1.take_dmg(59)
-print(hero1.health)
+class Orang:
+    
+    def __init__(self, nama, umur):
+        self.nama = nama
+        self.umur = umur
+        
+    def halo(self):
+        print(self.nama, self.umur)
+        
+class Siswa(Orang):
+    def __init__(self, nama, umur, nisn):
+        super().__init__(nama, umur)
+        self.nisn = nisn
+        
+    def info(self):
+        print(f"nama: {self.nama}\numur: {self.umur}\nnisn: {self.nisn}")
+        
+daru = Siswa("Daru", 20, 8080)
+daru.info()
