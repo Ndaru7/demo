c371a3ca2917fcafd293230ec9a6092e4b7e2a09
Ndaru <slametkopling076@gmail.com>
2024-12-27 11:18:34 +0700

    Menambahkan kode pada bagian Delete list

--- todo/todo.py
+++ todo/todo.py
@@ -16,39 +16,50 @@ while(1):
     print("3. delete list")
     print("4. exit app\n")
 
-    user_input = input("[SELECT]> ")
+    print("[INFO] Pilih menu berdasarkan angka")
+    user_input = input("[INPUT] ")
 
+    # Bagian Add todo list
     if user_input == "1":
-        print("[INFO] add list\n")
+        print("[INFO] ADD LIST")
         input_id = id
-        input_todo = input("Todo: ")
+        print("[INFO] Masukan todo list")
+        input_todo = input("[INPUT] ")
         input_status = "belum"
 
         database.update({input_id: [input_todo, input_status]})
         id += 1
-
+    
+    # Bagian Update todo list
     elif user_input == "2":
-        print("[INFO] update list\n")
+        print("[INFO] UPDATE LIST")
         print("[INFO] Masukan ID todo list yang ingin di Update")
-        update_id = int(input("[INPUT] id: "))
+        update_id = int(input("[INPUT] "))
         if update_id not in database:
-            print("[ERROR] ID tidak ada pada database")
-            break
+            print("[ERROR] ID tidak ada didatabase")
+            continue
+
         print("[INFO] Masukkan todo baru")
-        update_todo = input("[INPUT] Todo: ")
-        update_status = input("[INPUT] Status: ")
+        update_todo = input("[INPUT] ")
+        print("[INFO] Masukan status baru")
+        update_status = input("[INPUT] ")
         database.update({update_id: [update_todo, update_status]})
 
+    # Bagian Delete todo list
     elif user_input == "3":
-        print("[INFO] delete list\n")
+        print("[INFO] DELETE LIST")
+        print("[INFO] Masukan ID todo list yang ingin di Delete")
+        delete_id = int(input("[INPUT] "))
+        if delete_id not in database:
+            print("[ERROR] ID tidak ada didatabase")
+            continue
+
+        database.pop(delete_id)
     
+    # Bagian exit program
     elif user_input == "4":
         print("[INFO] exit app\n")
         break
 
     else:
-        print("[ERROR] Terjadi kesalahan\n")
-
-    exit_option = input("[SELECT] Apakah sudah selesai? (y/n): ")
-    if exit_option == "y" or exit_option == "y":
-        break
+        print("[ERROR] Terjadi kesalahan\n") 
